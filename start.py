54c0291eaf80ae8bcf9a072d24c10baf58f87e50
Gloridust <Gloridust@gmail.com>
2024-03-03 23:06:55 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,26 +1,22 @@
 import ollama
 
-def response_text(message):
-    # modelname = 'gemma-7b'
-    modelname = 'qwen:14b'
+def get_response(message):
+    model_name = 'qwen:14b'
     response = ollama.chat(
-        model = modelname, 
-        messages = message,
-        stream = False,
-        )
+        model=model_name, 
+        messages=message,
+        stream=False,
+    )
     output_text = response['message']['content']
-    message_list = response['message']
-    # Parse model output
-    return (output_text,message_list)
-    # for chunk in response_text:
-    #     yield chunk['message']['content']
+    received_message = response['message']
+    return output_text, received_message
 
-message_lists = []
+message_history = []
 while True:
     user_input_text = input("Input:")
     message = {'role': 'user', 'content': user_input_text}
-    message_lists.append(message)
-    output_text, message_list = response_text(message_lists)
-    message_lists.append(message_list)
+    message_history.append(message)
+    output_text, received_message = get_response(message_history)
+    message_history.append(received_message)
     print(output_text)
-    print(message_lists)
+    # print(message_history)
