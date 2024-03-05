bd92802f452c5888af0a8b096153248eff95e7cd
Gloridust <Gloridust@gmail.com>
2024-03-05 15:30:57 +0800

    Update start.py

--- start.py
+++ start.py
@@ -3,13 +3,12 @@ import whisper
 import pyaudio
 import wave
 import os
-
+import pyttsx3
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
 
 def record_audio(filename, duration=5, sample_rate=44100, chunk_size=1024, format=pyaudio.paInt16, channels=1):
     audio = pyaudio.PyAudio()
-    # 打开麦克风
+    # open microphone
     stream = audio.open(format=format,
                         channels=channels,
                         rate=sample_rate,
@@ -25,12 +24,12 @@ def record_audio(filename, duration=5, sample_rate=44100, chunk_size=1024, forma
 
     print("Recording finished.")
 
-    # 关闭麦克风
+    # close microphone
     stream.stop_stream()
     stream.close()
     audio.terminate()
 
-    # 保存录制的音频文件
+    # save audio
     with wave.open(filename, 'wb') as wf:
         wf.setnchannels(channels)
         wf.setsampwidth(audio.get_sample_size(format))
@@ -40,14 +39,14 @@ def record_audio(filename, duration=5, sample_rate=44100, chunk_size=1024, forma
     print("Audio saved as", filename)
 
 
-def asr():
+def asr(file):
     model = whisper.load_model("base")
-    result = model.transcribe("audio.wav")
+    result = model.transcribe(file)
     print(result["text"])
     return(result["text"])
 
 def get_response(message):
-    model_name = 'qwen:14b'
+    model_name = 'qwen:7b'
     response = ollama.chat(
         model=model_name, 
         messages=message,
@@ -64,16 +63,21 @@ def delete_file(filename):
     except FileNotFoundError:
         print("File", filename, "not found.")
 
+def tts(output_text):
+    pyttsx3.speak(output_text)
+
 if __name__ == "__main__":
+    ssl._create_default_https_context = ssl._create_unverified_context
     message_history = []
+
     while True:
-        record_audio("audio.wav", duration=5)
         # user_input_text = input("Input:")
-        user_input_text = asr()
+        record_audio("audio.wav", duration=5)
+        user_input_text = asr("audio.wav")
+        delete_file("audio.wav")
         message = {'role': 'user', 'content': user_input_text}
         message_history.append(message)
         output_text, received_message = get_response(message_history)
         message_history.append(received_message)
         print(output_text)
-        delete_file("audio.wav")
-        # print(message_history)
+        tts(output_text)
