8cf5f68b665b0fe098a271bdac5636e414f2419b
Gloridust <Gloridust@gmail.com>
2024-03-05 12:10:26 +0800

    Update test_whisper.py

--- test_whisper.py
+++ test_whisper.py
@@ -1,5 +1,8 @@
 import whisper
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 model = whisper.load_model("base")
 result = model.transcribe("audio.wav")
 print(result["text"])
