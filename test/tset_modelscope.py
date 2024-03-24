fed36dda0062ffe07a8140141599184406c9e8fb
Gloridust <Gloridust@gmail.com>
2024-03-24 15:53:09 +0800

    Update tset_modelscope.py

--- test/tset_modelscope.py
+++ test/tset_modelscope.py
@@ -2,7 +2,7 @@ from modelscope import AutoModelForCausalLM, AutoTokenizer
 from modelscope import GenerationConfig
 
 tokenizer = AutoTokenizer.from_pretrained("qwen/Qwen-7B-Chat-Int4", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("qwen/Qwen-7B-Chat-Int4", device_map="auto", trust_remote_code=True, fp16=True).eval()
+model = AutoModelForCausalLM.from_pretrained("qwen/Qwen-7B-Chat-Int4", device_map="auto", trust_remote_code=True).eval()
 model.generation_config = GenerationConfig.from_pretrained("qwen/Qwen-7B-Chat-Int4", trust_remote_code=True)
 
 
